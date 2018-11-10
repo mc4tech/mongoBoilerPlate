9979253e033f13d71cf7a49da203878b33d2bf40
mc4tech <chavezmeguel@yahoo.com>
2018-11-09 23:20:25 -0500

    added ajax post route for adding task to database and render afterwards

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -5,7 +5,28 @@ const render = function () {
     console.log("render");
     $.get('/api/example')
         .then(function (data) {
-            console.log(data);
+            data.forEach((element, i) => {
+                let date = new Date(data[i].createdAt); // dateStr you get from mongolet
+                let day = date.getDate();
+                let month = date.getMonth() + 1;
+                let year = date.getFullYear();
+                let dateStr = `${month}/${day}/${year}`;
+                htmlstr += `<ul>${element.todo} created at ${dateStr}</ul>`
+                $content.append(htmlstr);
+            });
         })
 }
+
+$('.submit').on('click', function (e) {
+    e.preventDefault();
+    const task = {
+        todo: $('#task').val().trim()
+    }
+    $.post('/api/addexample', {
+        data: task
+    }).then(function (data) {
+        $('#task').val('');
+        render();
+    })
+})
 render();
